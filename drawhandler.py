adb73de1c79d52f0e72bd89d0a491544c7afdf22
Kenan SUN <kenansun0@gmail.com>
2015-06-25 20:38:07 +0800

    [2015/06/25] Modified drawhandler.py, now 2 spaces for x direction to have the same scale for x/y axes

--- drawhandler.py
+++ drawhandler.py
@@ -15,13 +15,13 @@ class ConsolePaintHandler(object):
         self.y_max = y_max+1
         self.y_min = y_min
         self.scale = scale
-        self.buf = [' '] * self.y_max
+        self.buf = ['  '] * self.y_max
         for i in range(self.y_max):
-            self.buf[i] = [' '] * self.x_max
+            self.buf[i] = ['  '] * self.x_max
     def clear_buf(self):
         for i in range(self.y_max):
             for j in range(self.x_max):
-                self.buf[i][j] = ' '
+                self.buf[i][j] = '  '
     def draw_line(self, p1, p2):
         if p1[0] == p2[0]:
             # Horizontal
@@ -41,7 +41,7 @@ class ConsolePaintHandler(object):
             # Otherwise
             pass
     def draw_point(self, p):
-        self.buf[p[1]][p[0]] = '*'
+        self.buf[p[1]][p[0]] = '* '
     def paint(self):
         import platform
         import os
